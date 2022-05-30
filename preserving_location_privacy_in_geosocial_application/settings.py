8c94104763326af84fd653ba6dd4c4c056dafdb5
suryaanand nadendla <103498529+8919134556@users.noreply.github.com>
2022-05-30 11:41:56 +0530

    Update settings.py

--- preserving_location_privacy_in_geosocial_application/settings.py
+++ preserving_location_privacy_in_geosocial_application/settings.py
@@ -139,8 +139,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'suryaanand975@gmail.com'
-EMAIL_HOST_PASSWORD = 'Anand@123'
+EMAIL_HOST_USER = '<your_email_addres>'
+EMAIL_HOST_PASSWORD = '<Email_password>'
 EMAIL_USE_TLS = True
 
 BASE_COUNTRY = "UK"
