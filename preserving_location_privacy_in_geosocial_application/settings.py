216970bf730f591313f282c576fc66c3c919676d
suryaanand nadendla <103498529+8919134556@users.noreply.github.com>
2022-05-30 11:42:31 +0530

    Update settings.py

--- preserving_location_privacy_in_geosocial_application/settings.py
+++ preserving_location_privacy_in_geosocial_application/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(=9)eyqut86-6fmtln7*td*tolvr#$(bhbab9azi5w2)5i8k2y'
+SECRET_KEY = '**********************************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
